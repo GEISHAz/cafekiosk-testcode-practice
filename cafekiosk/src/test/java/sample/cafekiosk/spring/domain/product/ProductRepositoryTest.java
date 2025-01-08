279502bbb21842ec05de0b31a749f9b7cd37d591
GEISHAz <allmin9702@naver.com>
2025-01-09 00:58:51 +0900

    [BE] feat: Implement some of Order features & Write test code about Order #20250109

--- cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -23,7 +23,7 @@ public class ProductRepositoryTest {
 
     @DisplayName("원하는 판매상태를 가진 상품들을 조회한다.")
     @Test
-    void test(){
+    void findAllBySellingStatusIn(){
         // given
         Product product1 = Product.builder()
                 .productNumber("001")
@@ -50,6 +50,47 @@ public class ProductRepositoryTest {
 
         // when
         List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING,HOLD));
+
+        // then
+        assertThat(products)
+                .hasSize(2)
+                .extracting("productNumber","name","sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001","아메리카노",SELLING),
+                        tuple("002","카페라떼",HOLD)
+
+                );
+    }
+
+    @DisplayName("상품번호 리스트로 상품들을 조회한다.")
+    @Test
+    void findAllByProductNumberIn(){
+        // given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(ProductSellingStatus.HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(ProductType.BAKERY)
+                .sellingStatus(STOP_SELLING)
+                .name("피넛버터브레드")
+                .price(5000)
+                .build();
+        productRepository.saveAll(List.of(product1,product2,product3));
+
+        // when
+        List<Product> products = productRepository.findAllByProductNumberIn(List.of("001","002"));
 
         // then
         assertThat(products)
